367037f6b7f274174e0c083d88b5d034121cd182
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-09-13 19:48:26 +0500

    update shedules

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as pyplot
 import csv
 
-sp = []
-tp = []
-dv = []
-hg = []
-sec = []
+speed = []
+temperature = []
+pressure = []
+height = []
+second = []
 
-def rdr(filename,row1,lst):
+def readCSVFile(filename,row1,lst):
 	with open(filename,newline='') as csvfile:
 		reader = csv.DictReader(csvfile,delimiter=";")
 		for row in reader:
@@ -17,29 +17,31 @@ def rdr(filename,row1,lst):
 
 def createShedule(sp,tp,dv,hg,scd):
 
+	print(second)
+
 	pyplot.figure(figsize=(14, 14))
 
 	pyplot.subplot(2, 2, 1)
-	pyplot.plot(sp,scd)
+	pyplot.plot(scd,sp)
 	pyplot.title('Скорость', fontsize = 20)
 	pyplot.xlabel('секунды', fontsize=12)
 	pyplot.ylabel('скорость м/с', fontsize=12)
 
 	pyplot.subplot(2, 2, 2)
-	pyplot.plot(tp, scd, 'r')
+	pyplot.plot(scd, tp, 'r')
 	pyplot.title('Температура', fontsize=20)
 	pyplot.xlabel('секунды', fontsize=12)
 	pyplot.ylabel('температура', fontsize=12)
 
 
 	pyplot.subplot(2, 2, 3)
-	pyplot.plot(dv, scd, 'purple')
+	pyplot.plot(scd, dv, 'purple')
 	pyplot.title('Давление', fontsize = 20)
 	pyplot.xlabel('секунды', fontsize=12)
 	pyplot.ylabel('давление', fontsize=12)
 
 	pyplot.subplot(2, 2, 4)
-	pyplot.plot(hg,scd , 'g')
+	pyplot.plot(scd, hg, 'g')
 	pyplot.title('Высота', fontsize=20)
 	pyplot.xlabel('секунды', fontsize=12)
 	pyplot.ylabel('высота', fontsize=12)
@@ -47,10 +49,9 @@ def createShedule(sp,tp,dv,hg,scd):
 
 	pyplot.show()
 
-rdr("tests/test1.csv","sp",sp)
-rdr("tests/test1.csv","tp",tp)
-rdr("tests/test1.csv","dv",dv)
-rdr("tests/test1.csv","hg",hg)
-for i in range(len(sp)):
-	sec.append(i+1)
-createShedule(sp,tp,dv,hg,sec)
+readCSVFile("tests/test1.csv","speed", speed)
+readCSVFile("tests/test1.csv","temperature", temperature)
+readCSVFile("tests/test1.csv","pressure", pressure)
+readCSVFile("tests/test1.csv","height", height)
+readCSVFile('tests/test1.csv', "second" , second)
+createShedule(speed,temperature,pressure,height,second)
